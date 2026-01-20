a9886b1d9db42829ceffcc560f647caccf27bb5d
D.S.S.Gangadhar.Prathap <23A91A0416@aec.edu.in>
2026-01-21 01:06:20 +0530

    Time: 47 ms (67.45%), Space: 20.4 MB (30.47%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        s = set()
+        while head:
+            if head in s:
+                return True
+            s.add(head)
+            head = head.next
+        return False
